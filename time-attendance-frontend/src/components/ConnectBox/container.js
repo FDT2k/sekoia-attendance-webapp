2f92330e8c4a1609b10a5d431f9cf5b23d83a867
Fabien Di Tore <fabien@ditore.ch>
2020-01-30 10:58:39 +0100

    useEffect && useState in CmmectBox

--- time-attendance-frontend/src/components/ConnectBox/container.js
+++ time-attendance-frontend/src/components/ConnectBox/container.js
@@ -4,65 +4,66 @@ Links the subject form to the redux store
 
 */
 
-import React, {Component} from 'react';
+import React, {Component,useState,useEffect} from 'react';
 import _ from 'lodash'
 
-export default (Composed,_defaultProps={}) =>{
 
-  class Container extends Component{
-    constructor(props){
-      super(props)
-      this.state={
-        loading:false,
-        error:false,
-        formValues:{
-          api_url: 'http://192.168.13.27:3001',
-          host: '192.168.13.250',
-          port: '8069',
-          database: 'Sekoia4',
-          username: 'support@sekoia.ch',
-          password: '',
+const defaultFormValues= {
+  api_url: 'http://192.168.13.27:3001',
+  host: '192.168.13.250',
+  port: '8069',
+  database: 'Sekoia4',
+  username: 'support@sekoia.ch',
+  password: '',
+}
+
+
+const getConfig = ()=>{
+  return localStorage.getItem('config');
+}
+
+
+export default (Composed) =>{
+
+  return (props)=> {
+
+    const {authenticate,load_stored_config} = props;
+
+
+    const [formValues, setFormValues]         = useState(defaultFormValues);
+    const [isLoading,setLoading]              = useState(false);
+    const [isAuthenticated,setAuthenticated]  = useState(false);
+    const [error, setError]                   = useState(null);
+
+    useEffect(()=>{
+      if (!formValues){
+        let values = getConfig();
+        if(typeof values !== undefined && values !== null){
+          setFormValues(Object.assign({},formValues,JSON.parse(values)))
         }
       }
-    }
-
-
-    componentDidMount(){
-      let values = localStorage.getItem('config');
-      console.log(values)
-      if(typeof values !== undefined && values !== null){
-        console.log(values)
-        const newState= {formValues:{...this.state.formValues,...JSON.parse(values)}};
-        console.log(newState)
-        this.setState({formValues:newState.formValues})
-      }
-    }
-
-    handleSubmit(values){
-      this.props.authenticate(values).then(
+    })
+    const handleSubmit = (values)=>{
+      authenticate(values).then(
         result=>{
           localStorage.setItem('config',JSON.stringify(_.omit(values,['password'])))
           localStorage.setItem('token',result.token)
-          this.props.load_stored_config();
-
+          setAuthenticated(true)
+          load_stored_config();
       //    window.location.href='/app'
         }
       );
     }
+    return (
+      <React.Fragment>
+        { isLoading && "<h1>Loading</h1>"}
 
-    render(){
-
-      return (
-        <React.Fragment>
-          { this.state.loading && "Loading"}
-          { !this.state.loading  && !this.state.error && <Composed
-            handleSubmit={this.handleSubmit.bind(this)}
-            formValues={this.state.formValues}
-          />}
-        </React.Fragment>
-      );
-    }
+        { !isLoading  && !error && <Composed
+          handleSubmit={handleSubmit}
+          formValues={formValues}
+        />}
+      </React.Fragment>
+    );
   }
-  Container.defaultProps = _defaultProps
-  return Container
+
 }
