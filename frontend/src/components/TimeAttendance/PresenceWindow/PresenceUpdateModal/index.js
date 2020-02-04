dfd90640792051446b857738f3435dd5dc8b54ec
Quentin Queloz <quentin.queloz@gmail.com>
2020-02-04 12:23:37 +0100

    Affichage du succès du PIN (en cours)

--- frontend/src/components/TimeAttendance/PresenceWindow/PresenceUpdateModal/index.js
+++ frontend/src/components/TimeAttendance/PresenceWindow/PresenceUpdateModal/index.js
@@ -1,6 +1,6 @@
 import { message, Modal } from 'antd';
 import { UserPic, UserToggleString } from 'components/TimeAttendance/UsersGrid/User';
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { get_attendance, toggle as toggleActionCreator } from 'redux/Users/actions';
 import usePin from './pinHook';
@@ -12,12 +12,16 @@ export default function PresenceUpdateModal(props) {
 
     const { visible, handleClose, user, pinSize } = props;
 
+    const [success, setSuccess] = useState(false);
+
+
     const dispatch = useDispatch();
 
     //dispatch the redux action when the pin has been entered
     // REDUX is responsible to update the user state and refresh all the views
     const pinEnteredHandler = (pin) => dispatch(toggleActionCreator(user.id, pin))
         .then(() => dispatch(get_attendance(user.id)))
+        .then(() => setSuccess(true))
         .catch(showError)
         .finally(reset)
 
@@ -35,6 +39,22 @@ export default function PresenceUpdateModal(props) {
 
     const title = `Bienvenue ${user.name}`;
 
+    let content;
+
+    if (success) {
+        content = <Fragment>
+            <h1>Au revoir {user.name}</h1>
+            <h1>Passez une bonne après-midi !</h1>
+        </Fragment>
+    } else {
+        content = <Fragment>
+            <p>
+                Saisissez votre code PIN pour pointer à <UserToggleString user={user} />
+            </p>
+            <PinPad pinLength={pin.length} handleTypeKey={handleTypeKey} />
+        </Fragment>
+    }
+
     return (
         <Modal
             title={title}
@@ -45,14 +65,13 @@ export default function PresenceUpdateModal(props) {
             footer={null}
             width="fit-content"
             className="presence-update-modal"
+            destroyOnClose={true}
         >
             <p style={{ textAlign: "center" }}>
                 <UserPic user={user} />
             </p>
-            <p>
-                Saisissez votre code PIN pour pointer à <UserToggleString user={user} />
-            </p>
-            <PinPad pinLength={pin.length} handleTypeKey={handleTypeKey} />
+
+            {content}
         </Modal>
     )
 }
