57ef41c93ff5680f4854130eb5888bdd972c2372
Fabien Di Tore <fabien@ditore.ch>
2020-02-05 16:09:38 +0100

    Peinture

--- frontend/src/components/TimeAttendance/Header/index.js
+++ frontend/src/components/TimeAttendance/Header/index.js
@@ -8,16 +8,18 @@ export default props => {
 
   const handleVisibility = props.handleVisibility
     return (
-      <Layout.Header style={{ zIndex: 1, width: '100%', background: 'none' }}>
+      <Layout.Header style={{ zIndex: 1, width: '100%', background: 'none', color:'white' }}>
 
         <PageHeader
-            style={{
-                border: '1px solid black',
-            }}
             title="Sekoia"
             subTitle="Pointage pour la présence"
+            style={{
+              backgroundColor:'#0AABB5',
+              marginTop: '20px',
+              color:'white'
+            }}
             extra={[
-              <Button onTouchStart={handleVisibility} onTouchEnd={handleVisibility} key="showPresents">Afficher les présents</Button>,
+              <Button style={{backgroundColor:'rgba(0,0,0,0.1)',color:'white',border:'0px', paddingRight:'20px;'}} onTouchStart={handleVisibility} onTouchEnd={handleVisibility} key="showPresents">Afficher les présents</Button>,
               <Clock key="clock" />
             ]}
         />
